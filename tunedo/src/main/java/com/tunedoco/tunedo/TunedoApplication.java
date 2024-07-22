7a27cdb02e988570338d1e1fa24707a5a1e99968
ZBishop <obisporicapa@gmail.com>
2024-07-22 11:53:13 -0500

    TDT-12 Project initialization

---
+++ tunedo/src/main/java/com/tunedoco/tunedo/TunedoApplication.java
@@ -0,0 +1,13 @@
+package com.tunedoco.tunedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunedoApplication.class, args);
+	}
+
+}
